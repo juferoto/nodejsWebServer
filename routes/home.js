ee09c162f6fc5f15ef3fa41e342c1816aafaee75
Juan Felipe <jnferoto@gmail.com>
2019-06-22 07:03:04 -0500

    Cambios para mostrar las noticias

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,8 @@
+const connection = require("../mysql.conf").connection;
 const express = require('express');
 const app = express();
 
-let news = [
+/*let news = [
     { 
         title: "Alerta: se acaba el curso", 
         category: "Technlogy", 
@@ -14,9 +15,16 @@ let news = [
         date: "June 15, 2019", 
         content: "Tenemos que elegir el sitio para ir a almorzar la otra semana. Nota: Redactor disléxico." 
     }
-]
+]*/
 
 app.get('/', function (req, res) {
+    let news;
+    connection.connect();
+    connection.query('SELECT * FROM news', (error, results, fields) => {
+        if (error) { throw error };
+        connection.end();
+        res.json(results);
+    });
     res.render('index/index', {
         title: "El Profeta | Home",
         news
